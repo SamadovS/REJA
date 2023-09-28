83ac76f620c034e909dacdd33ebae9994f30ea23
Sirojiddin <siroj.origin.profile>
2023-09-28 12:38:45 +0900

    lesson: 19: callback function

--- train.js
+++ train.js
@@ -1,1 +1,31 @@
-console.log("TRAIN AREA");
+console.log("Jack Ma maslahatlari");
+const list = [
+    "yaxshi talaba bo'ling", // 0-20
+    "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+    "o'zingizga ishlashni boshlang", // 30-40
+    "siz kuchli bo'lgan narsalarni qiling", // 40-50
+    "yoshlarga investitsiya qiling", // 50-60
+    "endi dam oling, foydasi yo'q endi", // 60-70
+];
+
+function maslahatBering(a, callback) {
+    if (typeof a !== "numb")
+        callback("insert a number", null); //1-qismi: error    2-qismi: data
+    else if (a <= 20) callback(null, list[0]);
+    else if (a > 20 && a <= 30) callback(null, list[1]);
+    else if (a > 30 && a <= 40) callback(null, list[2]);
+    else if (a > 40 && a <= 50) callback(null, list[3]);
+    else if (a > 50 && a <= 60) callback(null, list[4]);
+    else {
+        setTimeout(function () {
+            callback(null, list[5]);
+        }, 5000);
+    }
+}
+console.log("passed here 0");
+// parametr sifatida function ishga tushadi
+maslahatBering(65, (err, data) => {
+    if (err) console.log("ERROR: ", err);
+    console.log("Javob:", data);
+});
+console.log("passed here 1");
