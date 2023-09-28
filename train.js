14964febe6acd30ec2390d59c1346cc4818152d7
Sirojiddin <siroj.origin.profile>
2023-09-28 14:02:35 +0900

    lesson 19-20: callback and async functions

--- train.js
+++ train.js
@@ -1,3 +1,5 @@
+// SYNCHRONOUS (CALLBACK) FUNCTION
+console.log("<<< SYNCHRONOUS (callback) FUNCTION >>>");
 console.log("Jack Ma maslahatlari");
 const list = [
     "yaxshi talaba bo'ling", // 0-20
@@ -9,7 +11,7 @@ const list = [
 ];
 
 function maslahatBering(a, callback) {
-    if (typeof a !== "numb")
+    if (typeof a !== "number")
         callback("insert a number", null); //1-qismi: error    2-qismi: data
     else if (a <= 20) callback(null, list[0]);
     else if (a > 20 && a <= 30) callback(null, list[1]);
@@ -29,3 +31,53 @@ maslahatBering(65, (err, data) => {
     console.log("Javob:", data);
 });
 console.log("passed here 1");
+console.log(
+    "==================================================================================================================="
+);
+
+// ASYNCHRONOUS FUNCTION
+console.log("<<< ASYNCHRONOUS FUNCTION >>>");
+async function maslahatBer(a) {
+    if (typeof a !== "number")
+        throw new Error("insert a number"); //1-qismi: error    2-qismi: data
+    else if (a <= 20) return list[0];
+    else if (a > 20 && a <= 30) return list[1];
+    else if (a > 30 && a <= 40) return list[2];
+    else if (a > 40 && a <= 50) return list[3];
+    else if (a > 50 && a <= 60) return list[4];
+    else {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            }, 5000);
+        });
+    }
+}
+// sen yuqorida async func ishlatding, log qismida emas
+// we use: callback and promise-hell
+// // then()   catch()  ==> *** async yozayotganda paytda buni ishlatishga harakat qil
+// console.log("passed here 0"); // bu sync function ==> bu 1-bajariladi (bu single threadni o'zida bajariladi)
+// maslahatBer(25)
+//     .then((data) => {
+//         console.log("Javob:", data);
+//     }) // bu "async" function ==> bu 3-bajariladi (oxiri bajariladi)
+//     .catch((err) => {
+//         console.log("Error:", err); // lekin bu WORKERS POOL ga bir tushib chiqadi
+//     });
+// console.log("passed here 1"); // bu sync function ==> bu 2-bajariladi (bu ham single threadni o'zida bajariladi)
+
+// log qismida ham async func ishlatish:
+// async/await ()
+async function run() {
+    let javob = await maslahatBer(20);
+    console.log(javob);
+    javob = await maslahatBer(70);
+    console.log(javob);
+    javob = await maslahatBer(42);
+    console.log(javob);
+    javob = await maslahatBer(53);
+    console.log(javob);
+}
+run();
+
+// promise function
