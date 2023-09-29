99e9013eec59d614d36b4d0faf20b4f0dbab4989
Sirojiddin <siroj.origin.profile>
2023-09-29 10:11:56 +0900

    feat: connect MongoDB

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log("Starting Web Server");
+
+// install express -->npm i express     Below: Building Backend Server using Nodejs Framework - Express
+const express = require("express");
+const app = express();
+
+// Connect/call MongoDB
+const db = require("./server").db();
+
+// 1: Entry codes
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session codes
+
+// 3: Views codes        (We selected BSSR-traditional way)  // install ejs -->npm i ejs
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing codes
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+    res.render("reja");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,49 +1,31 @@
-console.log("Starting Web Server");
-
-// install express -->npm i express     Below: Building Backend Server using Nodejs Framework - Express
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs"); // ==> fs = file system (core module-package, you don't need install it)
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if (err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data);
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+    "mongodb+srv://sirojoriginprofile:WP0xjS70OzyF6DZb@cluster0.ylmjatw.mongodb.net/";
+
+mongodb.connect(
+    connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if (err) console.log("Error on connection MongoDB");
+        else {
+            console.log("MongoDB connection succeed");
+            module.exports = client;
+            // console.log(client);
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function () {
+                console.log(
+                    `This server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+                );
+            });
+        }
     }
-});
-
-// 1: Entry codes
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2: Session codes
-
-// 3: Views codes        (We selected BSSR-traditional way)  // install ejs -->npm i ejs
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4: Routing codes
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-    res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-    res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(
-        `This server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
-    );
-});
+);
