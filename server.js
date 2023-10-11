25cc3bffd4c923038f15e31e68f4f51c3cdbbc8e
Sirojiddin <siroj.origin.profile>
2023-10-11 22:12:42 +0900

    feat: dotenv & project configuration

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const http = require("http");
 const mongodb = require("mongodb");
 
-let db;
-const connectionString =
-    "mongodb+srv://sirojoriginprofile:WP0xjS70OzyF6DZb@cluster0.ylmjatw.mongodb.net/Reja";
+const connectionString = process.env.MONGO_URL;
 
 mongodb.connect(
     connectionString,
@@ -19,7 +19,7 @@ mongodb.connect(
             // console.log(client);
             const app = require("./app");
             const server = http.createServer(app);
-            let PORT = 5500;
+            let PORT = process.env.PORT || 5500;
             server.listen(PORT, function () {
                 console.log(
                     `This server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
