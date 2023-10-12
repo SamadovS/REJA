8a3f325f161e07c4075b47d760e540efa5e4aa03
Sirojiddin <siroj.origin.profile>
2023-10-12 18:54:49 +0900

    fix: completed task

--- train.js
+++ train.js
@@ -1,1 +1,11 @@
-// sam
+// I-Task: Shunday function tuzing,
+// u bir array argument qilib qabul qilib, osha arrayning 0 index qiymatni arrayning oxiriga qoyib return qilsin
+// masalan: getCompute(['h', 'e', 'l', 'l', 'o']) return qilishi kerak ['e', 'l', 'l', 'o', 'h']
+
+function getCompute(a) {
+    letter = a.shift();
+    console.log(letter);
+    result = a.concat(letter);
+    console.log(result);
+}
+getCompute(["h", "e", "l", "l", "o"]);
